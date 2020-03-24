c1d44f67f9796f45a92f9d1b841ce82ec4eb863a
j.bogg <jonathan.bogg@gmail.com>
2020-03-24 12:08:59 +0100

    add styling

--- my-app/src/HomePage.tsx
+++ my-app/src/HomePage.tsx
@@ -9,35 +9,49 @@ class HomePage extends React.Component {
         return (
             <Siteframe {...{
                 headerProps: {
-                    className: 'bg-light-gray type-black',
-                    logo: <div className="ptl pbl pll" style={{ fontSize: '40px' }}></div>,
-                    companyName: 'RetroShop',
+                    className: 'bg-light-gray paxl',
+                    companyName: <span style={{ color: 'black' }}>RetroShop</span>,
                 },
                 sidebarProps: {
                     className: 'bg-decorative-yellow',
                     primaryLinks: [
-                        { text: 'Product 1' },
-                        { text: 'Product 2' },
-                        { text: 'Product 3' },
-                        { text: 'Product 4' },
-                        { text: 'Product 5' },
-                        { text: 'Product 6' },
+                        { text: 'T-shirt' },
+                        { text: 'Jacket' },
+                        { text: 'Sneakers' },
+                        { text: 'Sweatshirt' },
+                        { text: 'Shirt' },
+                        { text: 'Jeans' },
                     ],
                     secondaryLinks: [{ text: 'Checkout' }, { text: 'Contact' }],
-                    renderLink: ({ text }) => <a style={{ color: 'unset' }} href="#">{text}</a>
+                    renderLink: ({ text }) => <a className="underline" style={{ color: 'unset' }} href="#">{text}</a>
                 }
             }}>
                 <div className="bg-light-green paxl" style={{ height: '100%', overflow: 'scroll' }}>
-                    <Panel {...{ header: <h3 className="mam">All products</h3> }}>
-                        <Grid className="grid-show mbxl pal">
-                            <FlexCol col={8}>
-                                <Image style={{ height: '100%', width: '100%', objectFit: 'cover' }} src="http://source.unsplash.com/user/malvestida/Rp-viEAP8Bo" />
+                    <Panel {...{
+                        header: <h3 className="mam">All products</h3>,
+                        className: 'width-product', panelClassName: 'width-product'
+                    }}>
+                        <Grid className="grid-show type-sm pbxl">
+                            <FlexCol col={8} className="width-product pan mbxxxl">
+                                <Image className="pal" src="http://source.unsplash.com/user/malvestida/Rp-viEAP8Bo" />
+                                <p className="pll prl mbxl man display-flex space-between">
+                                    <span>Nike Air</span>
+                                    <span>599SEK</span>
+                                </p>
                             </FlexCol>
-                            <FlexCol col={8}>
-                                <Image style={{ height: '100%', width: '100%', objectFit: 'cover' }} src="http://source.unsplash.com/user/ryancp/EGOclsi5QZY" />
+                            <FlexCol col={8} className="width-product pan mbxxxl">
+                                <Image className="pal" src="http://source.unsplash.com/user/ryancp/EGOclsi5QZY" />
+                                <p className="pll prl mbxl man display-flex space-between">
+                                    <span>Adidas</span>
+                                    <span>899SEK</span>
+                                </p>
                             </FlexCol>
-                            <FlexCol col={8}>
-                                <Image style={{ height: '100%', width: '100%', objectFit: 'cover' }} src="http://source.unsplash.com/user/tamasp/_PLpBPi6IB4" />
+                            <FlexCol col={8} className="width-product pan mbxxxl">
+                                <Image className="pal" src="http://source.unsplash.com/user/tamasp/_PLpBPi6IB4" />
+                                <p className="pll prl mbxl man display-flex space-between">
+                                    <span>Vans</span>
+                                    <span>499SEK</span>
+                                </p>
                             </FlexCol>
                         </Grid>
                     </Panel>
