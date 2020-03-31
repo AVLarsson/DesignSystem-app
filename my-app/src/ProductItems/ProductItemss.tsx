0ab60eb64bd44dd64bdddbde7212b6c6534c955b
j.bogg <jonathan.bogg@gmail.com>
2020-03-31 17:28:23 +0200

    fix error and clean up code

--- my-app/src/ProductItems/ProductItemss.tsx
+++ my-app/src/ProductItems/ProductItemss.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Modal } from 'pivotal-ui/react/modal';
 import { mockedProducts } from '../MockedData'
 // Importera från alla sidorna (skulle jag tro!)
 
@@ -24,23 +25,24 @@ function ProductItem(props: Props) {
     const addToCart = () => {
         props.handleAdd(props.product)
     }
-}
 
-const {name, description, price, image} = props.product
-return (
-    <div className='Products'>
-        <img alt='' src={image} onClick={() => 
-            setShow(true)} />
-        <p className='productTitle'>{name}</p>
-        <p className='prodictDescription'>{description}</p>
-        <p className='productPrice'>{price}</p>
-        <button id='addToCart' onClick={addToCart}>Köp</button>
 
-        <Modal show={show}  setShow={setShow}>
-            <img alt='' src={image} className='product-modal-image' />
+    const { name, description, price, image } = props.product
+    return (
+        <div className='Products'>
+            <img alt='' src={image} onClick={() =>
+                setShow(true)} />
             <p className='productTitle'>{name}</p>
-        </Modal>
+            <p className='prodictDescription'>{description}</p>
+            <p className='productPrice'>{price}</p>
+            <button id='addToCart' onClick={addToCart}>Köp</button>
 
-    </div>
-    
-)
+            <Modal show={show} setShow={setShow}>
+                <img alt='' src={image} className='product-modal-image' />
+                <p className='productTitle'>{name}</p>
+            </Modal>
+
+        </div>
+
+    )
+}
