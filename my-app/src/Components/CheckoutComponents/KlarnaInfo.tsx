0b5a1256e63af6802c67e88a186b273eedbef517
Jonathan G. Bogg <jonathan.bogg@gmail.com>
2020-05-28 10:48:16 +0200

    add Form to all payment components

--- my-app/src/Components/CheckoutComponents/KlarnaInfo.tsx
+++ my-app/src/Components/CheckoutComponents/KlarnaInfo.tsx
@@ -1,23 +1,39 @@
 import * as React from 'react';
 import "../Imports.css";
-import {Input} from 'pivotal-ui/react/inputs';
+import { Input } from 'pivotal-ui/react/inputs';
+import { Grid, FlexCol } from 'pivotal-ui/react/flex-grids';
+import { Form } from 'pivotal-ui/react/forms';
+import { Panel } from 'pivotal-ui/react/panels';
+import ConfirmOrderButton from '../ConfirmOrderButton';
+import { CartContext } from '../CartContext';
+
+// eslint-disable-next-line
+const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 interface Props {
-  passStateFromKlarna: any
+  passStateFromKlarna: (props: any) => void
+  checkIfDone: () => boolean
 }
 
-export default class KlarnaInfo extends React.Component  <Props, {}> {
+interface State {
+  firstName: string
+  lastName: string
+  email: string
+}
 
-  constructor(props:any) {
+export default class KlarnaInfo extends React.Component<Props, State> {
+  static contextType = CartContext;
+  constructor(props: any) {
     super(props)
-    this.checkIfNumber = this.checkIfNumber.bind(this)
-}
 
-state = {
-    firstName: "",
-    lastName: "",
-    email: ""
-}
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: ""
+    }
+
+    // this.checkIfNumber = this.checkIfNumber.bind(this)
+  }
 
   // componentDidMount = () => {
 
@@ -26,7 +42,7 @@ state = {
   //   let userEmail;
 
 
-    
+
   //   if (localStorage.firstName){
   //       userFirstName = JSON.parse(localStorage.firstName);
   //       this.setState({firstName:userFirstName})
@@ -40,46 +56,99 @@ state = {
   //       this.setState({email:userEmail})
   //   }
   // }
-
-  checkIfNumber(event:any) {
+/*
+  checkIfNumber(event: any) {
     event.persist()
     let targetValue = event.target.value
     const targetName = event.target.name;
 
-    const regex=/^[a-zA-Z]+$/;
+    const regex = /^[a-zA-Z]+$/;
 
     if (targetName !== "email") {
       for (let i = 0; i < event.target.value.length; i++) {
-          if (!targetValue[i].match(regex))
-          {
-              event.target.value = "";
-              targetValue = "";
-          }
+        if (!targetValue[i].match(regex)) {
+          event.target.value = "";
+          targetValue = "";
+        }
       }
     }
 
 
-          if (targetName === "firstName") {
-            this.setState({firstName:targetValue}, this.sendToParent)
-        }
+    if (targetName === "firstName") {
+      this.setState({ firstName: targetValue }, this.sendToParent)
+    }
 
-        if (targetName === "lastName") {
-            this.setState({lastName:targetValue}, this.sendToParent)
-        }
+    if (targetName === "lastName") {
+      this.setState({ lastName: targetValue }, this.sendToParent)
+    }
 
-        if (targetName === "email") {
-            this.setState({email:targetValue}, this.sendToParent)
-        }
-}
+    if (targetName === "email") {
+      this.setState({ email: targetValue }, this.sendToParent)
+    }
+  }
+  */
 
-sendToParent = () => {
-  this.props.passStateFromKlarna(this.state)
-}
+  checkIfEmail(input: any) {
+    const regex = new RegExp(EMAIL_REGEX);
+    return regex.test(input);
+  }
 
+  sendToParent = (props: State) => {
+    this.props.passStateFromKlarna(props)
+  }
+
+  handleSubmit({ firstName, lastName, email }: any) {
+    this.sendToParent({ firstName, lastName, email })
+  }
 
   render() {
     return (
-
+      <Panel>
+        <Form {...{
+          className: "pbxxxl mbxxl",
+          onSubmit: ({ initial, current }: any) => { this.handleSubmit(current) },
+          fields: {
+            firstName: {
+              initialValue: '',
+              validator: (currentValue: any) => currentValue.length < 1 ? 'Please enter your first name' : null,
+              label: 'First Name'
+            },
+            lastName: {
+              initialValue: '',
+              validator: (currentValue: any) => currentValue.length < 1 ? 'Please enter your last name' : null,
+              label: 'Last Name'
+            },
+            email: {
+              initialValue: '',
+              validator: (currentValue: any) => !this.checkIfEmail(currentValue) ? 'Please enter a valid email as abc@abc.com' : null,
+              label: 'Email',
+              help: 'Hint: abc@abc.com'
+            }
+          }
+        }}>
+          {({ fields, canSubmit, onSubmit }: any) => {
+            return (
+              <div>
+                <FlexCol>
+                  <Grid>
+                    <FlexCol>{fields.firstName}</FlexCol>
+                    <FlexCol>{fields.lastName}</FlexCol>
+                  </Grid>
+                  <Grid>
+                    <FlexCol>{fields.email}</FlexCol>
+                  </Grid>
+                  <Grid>
+                    <FlexCol>
+                      <ConfirmOrderButton onClick={() => canSubmit() ? onSubmit() : null} isDisabled={!canSubmit() || !this.props.checkIfDone} />
+                    </FlexCol>
+                  </Grid>
+                </FlexCol>
+              </div>
+            );
+          }}
+        </Form>
+      </Panel>
+      /*
       <div>
         <form style={this.gridContainer}>
           <div style={this.gridItem}>
@@ -96,19 +165,7 @@ sendToParent = () => {
           </div>
         </form>
       </div>
+      */
     )
   }
-
-  gridContainer: React.CSSProperties = {
-    display: "grid",
-    gridTemplateColumns: "auto auto",
-    gridColumnGap: "50px",
-    gridRowGap: "20px",
-    justifyContent: "center",
-  }
-  
-  gridItem: React.CSSProperties = {
-      fontSize: "16px"
-  }
-
 }
--- my-app/src/Components/CheckoutComponents/SwishInfo.tsx
+++ my-app/src/Components/CheckoutComponents/SwishInfo.tsx
@@ -1,125 +1,188 @@
 import * as React from 'react';
-import {Input} from 'pivotal-ui/react/inputs';
+// import { Input } from 'pivotal-ui/react/inputs';
 import "../Imports.css";
+import { Grid, FlexCol } from 'pivotal-ui/react/flex-grids';
+import { Form } from 'pivotal-ui/react/forms';
+import { Panel } from 'pivotal-ui/react/panels';
+import ConfirmOrderButton from '../ConfirmOrderButton';
+import { CartContext } from '../CartContext';
 
 interface Props {
-    passStateFromSwish: any
+  passStateFromSwish: any
+  checkIfNumber: (input: string) => boolean
+  checkIfDone: () => boolean | undefined
+}
+interface State {
+  firstName: string
+  lastName: string
+  phoneNumber: string
+}
+
+
+export default class SwishInfo extends React.Component<Props, State> {
+  static contextType = CartContext;
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      firstName: "",
+      lastName: "",
+      phoneNumber: ""
+    }
+
   }
-  
 
-export default class SwishInfo extends React.Component <Props, {}> {
+  //   componentDidMount = () => {
 
-    constructor(props:any) {
-        super(props)
-        this.checkIfNumber = this.checkIfNumber.bind(this)
-    }
-    
-    state = {
-        firstName: "",
-        lastName: "",
-        phoneNumber: ""
-    }
-
-//   componentDidMount = () => {
-
-//     let userFirstName; 
-//     let userLastName;
-//     let userPhoneNumber;
-
-    
-//     if (localStorage.firstName){
-//         userFirstName = JSON.parse(localStorage.firstName);
-//         this.setState({firstName:userFirstName})
-//     }
-//     if (localStorage.lastName){
-//         userLastName = JSON.parse(localStorage.lastName);
-//         this.setState({lastName:userLastName})
-//     }
-//     if (localStorage.phoneNumber){
-//         userPhoneNumber = JSON.parse(localStorage.phoneNumber);
-//         this.setState({phoneNumber:userPhoneNumber})
-//     }
-// }
+  //     let userFirstName; 
+  //     let userLastName;
+  //     let userPhoneNumber;
 
 
-checkIfNumber(event:any) {
+  //     if (localStorage.firstName){
+  //         userFirstName = JSON.parse(localStorage.firstName);
+  //         this.setState({firstName:userFirstName})
+  //     }
+  //     if (localStorage.lastName){
+  //         userLastName = JSON.parse(localStorage.lastName);
+  //         this.setState({lastName:userLastName})
+  //     }
+  //     if (localStorage.phoneNumber){
+  //         userPhoneNumber = JSON.parse(localStorage.phoneNumber);
+  //         this.setState({phoneNumber:userPhoneNumber})
+  //     }
+  // }
+
+/*
+  checkIfNumber(event: any) {
     event.persist()
     let targetValue = event.target.value
     let targetId = event.target.id;
     const targetName = event.target.name;
-    const regex=/^[a-zA-Z]+$/;
+    const regex = /^[a-zA-Z]+$/;
 
 
     if (targetId === "userPhoneNumberSwish") {
-    
-        for (let i = 0; i < event.target.value.length; i++) {
-            if (targetValue[i].match(regex))
-            {
-                event.target.value = "";
-            }
+
+      for (let i = 0; i < event.target.value.length; i++) {
+        if (targetValue[i].match(regex)) {
+          event.target.value = "";
         }
+      }
     }
     else {
-        for (let i = 0; i < event.target.value.length; i++) {
-            if (!targetValue[i].match(regex))
-            {
-                event.target.value = "";
-            }
+      for (let i = 0; i < event.target.value.length; i++) {
+        if (!targetValue[i].match(regex)) {
+          event.target.value = "";
         }
+      }
     }
 
 
     if (targetName === "firstName") {
-        this.setState({firstName:targetValue}, this.sendToParent)
+      this.setState({ firstName: targetValue }, this.sendToParent)
     }
 
     if (targetName === "lastName") {
-        this.setState({lastName:targetValue}, this.sendToParent)
+      this.setState({ lastName: targetValue }, this.sendToParent)
     }
 
     if (targetName === "phoneNumber") {
-        this.setState({phoneNumber:targetValue}, this.sendToParent)
+      this.setState({ phoneNumber: targetValue }, this.sendToParent)
     }
-}
+  }
+  */
 
-sendToParent = () => {
-    this.props.passStateFromSwish(this.state)
+  sendToParent = (props: State) => {
+    this.props.passStateFromSwish(props)
   }
 
+  handleSubmit = ({ firstName, lastName, phoneNumber }: State) => {
+    this.sendToParent({ firstName, lastName, phoneNumber })
+  }
 
-    render() {
-      return (
-        <div>
-        <form style={this.gridContainer} action="">
-            <div style={this.gridItem}>
-                <label htmlFor="userFirstNameSwish">First Name</label>
-                <Input name="firstName" id="userFirstNameSwish" onChange={this.checkIfNumber}/>
-            </div>
-            <div style={this.gridItem}>
-                <label htmlFor="userLastNameSwish">Last Name</label>
-                <Input name="lastName" id="userLastNameSwish" onChange={this.checkIfNumber}/>
-            </div>
-            <div style={this.gridItem}>
-                <label htmlFor="userPhoneNumberSwish">Phone Number</label>
-                <Input name="phoneNumber" id="userPhoneNumberSwish" onChange={this.checkIfNumber}/>
-            </div>
-        </form>
-    </div>
+  render() {
+    return (
+
+      <Panel>
+        <Form {...{
+          resetOnSubmit: false,
+          className: "pbxxxl mbxxl",
+          onSubmit: ({ initial, current }: any) => { this.handleSubmit(current) },
+          fields: {
+            firstName: {
+              initialValue: '',
+              validator: (currentValue: any) => currentValue.length < 1 ? 'Please enter your first name' : null,
+              label: 'First Name'
+            },
+            lastName: {
+              initialValue: '',
+              validator: (currentValue: any) => currentValue.length < 1 ? 'Please enter your last name' : null,
+              label: 'Last Name'
+            },
+            phoneNumber: {
+              initialValue: '',
+              validator: (currentValue: any) => !this.props.checkIfNumber(currentValue) ? 'Please enter a valid email' : null,
+              label: 'Phone Number',
+            }
+          }
+        }}>
+          {({ fields, canSubmit, onSubmit }: any) => {
+            return (
+              <div>
+                <FlexCol>
+                  <Grid>
+                    <FlexCol>{fields.firstName}</FlexCol>
+                    <FlexCol>{fields.lastName}</FlexCol>
+                  </Grid>
+                  <Grid>
+                    <FlexCol>{fields.phoneNumber}</FlexCol>
+                  </Grid>
+                  <Grid>
+                    <FlexCol>
+                      <ConfirmOrderButton onClick={() => canSubmit() ? onSubmit() : null} isDisabled={!canSubmit() || !this.props.checkIfDone} />
+                    </FlexCol>
+                  </Grid>
+                </FlexCol>
+              </div>
+            );
+          }}
+        </Form>
+
+      </Panel >
+      /*
+      <div>
+      <form style={this.gridContainer} action="">
+          <div style={this.gridItem}>
+              <label htmlFor="userFirstNameSwish">First Name</label>
+              <Input name="firstName" id="userFirstNameSwish" onChange={this.checkIfNumber}/>
+          </div>
+          <div style={this.gridItem}>
+              <label htmlFor="userLastNameSwish">Last Name</label>
+              <Input name="lastName" id="userLastNameSwish" onChange={this.checkIfNumber}/>
+          </div>
+          <div style={this.gridItem}>
+              <label htmlFor="userPhoneNumberSwish">Phone Number</label>
+              <Input name="phoneNumber" id="userPhoneNumberSwish" onChange={this.checkIfNumber}/>
+          </div>
+      </form>
+  </div>
+  */
     )
-    }
-    centerStyle: React.CSSProperties = {
-      display: "flex",
-      justifyContent: "center",
-      paddingTop: "20px"
-    }
-    gridContainer: React.CSSProperties = {
-      display: "grid",
-      gridTemplateColumns: "auto auto",
-      gridColumnGap: "50px",
-      gridRowGap: "20px",
-      justifyContent: "center",
+  }
+
+  centerStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "center",
+    paddingTop: "20px"
+  }
+  gridContainer: React.CSSProperties = {
+    display: "grid",
+    gridTemplateColumns: "auto auto",
+    gridColumnGap: "50px",
+    gridRowGap: "20px",
+    justifyContent: "center",
   }
   gridItem: React.CSSProperties = {
-      fontSize: "16px"
+    fontSize: "16px"
   }
 }
